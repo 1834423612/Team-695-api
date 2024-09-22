639f9d7238aff710b164afac0d8cbc5b5c9dbd50
kjch <1834423612@qq.com>
2024-09-22 04:48:03 +0000

    feat: database change to pool & db keep alive strategy

--- config/database.ts
+++ config/database.ts
@@ -1,21 +1,29 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
+// 导入 .env 变量
 dotenv.config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
+    connectionLimit: 100, // 最大连接数
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Database connection error: ', err);
-        return;
-    }
-    console.log('Connected to the database.');
-});
+// 定时刷新连接，防止长时间不活跃导致连接关闭
+setInterval(() => {
+    const startTime = new Date();
+    pool.query('SELECT 1', (err) => {
+        const endTime = new Date();
+        const executionTime = endTime.getTime() - startTime.getTime();
+        if (err) {
+            console.error(`[${startTime.toISOString()}] Keep-alive query failed:`, err);
+        } else {
+            console.log(`[${startTime.toISOString()}] Keep-alive query executed successfully in ${executionTime}ms`);
+        }
+    });
+}, 3600000); // 每小时执行一次
 
-export default db;
+export default pool;
