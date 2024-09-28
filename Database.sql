b2473b3cca24cf2ef3b1f849b707098cdbeca8ae
kjch <1834423612@qq.com>
2024-09-28 05:40:51 +0000

    feat: DB unique index for table "events"

--- Database.sql
+++ Database.sql
@@ -9,14 +9,13 @@ CREATE TABLE feedback (
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-DROP TABLE IF EXISTS survey_questions;
-
 CREATE TABLE events (
     id INT AUTO_INCREMENT PRIMARY KEY,
     event_id VARCHAR(255) NOT NULL,
     event_name VARCHAR(255) NOT NULL,
     event_date DATE NOT NULL,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE INDEX (event_id) -- 为 event_id 添加唯一索引
 );
 
 CREATE TABLE teams (
